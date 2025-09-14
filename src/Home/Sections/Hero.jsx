365ff9f75f3961973c94ad40673eaa018780cdda
Yeassin7376 <yeassinali17@gmail.com>
2025-09-15 01:22:48 +0600

    updated some style in hero section

--- src/Home/Sections/Hero.jsx
+++ src/Home/Sections/Hero.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
 import { FaFacebookSquare, FaGithubSquare, FaLinkedin } from 'react-icons/fa';
+import { ReactTyped } from 'react-typed';
 
 const Hero = () => {
   return (
     <div id="home" className="w-10/12 mx-auto py-5 md:py-10  ">
-      <div className="flex items-center justify-center  flex-col lg:justify-between lg:flex-row-reverse">
-        <img src="https://i.ibb.co.com/mr7vbkzZ/profile-picture-9-min.png" className="max-w-[400px] h-[400px] p-1 border-s-2 border-e-2 border-yellow-200  rounded-full object-cover shadow-2xl" />
-        <div className="flex flex-col items-center md:items-start gap-10">
+      <div className="flex items-center  justify-center lg:justify-end lg:gap-30  flex-col md:flex-row-reverse ">
+        <div class="relative inline-block">
+          {/* <!-- Glow layer --> */}
+          <div class="absolute inset-0 rounded-full bg-gradient-to-r from-yellow-300 via-white to-yellow-300 blur-3xl opacity-30"></div>
+
+          {/* <!-- Profile photo --> */}
+          <img src="https://i.ibb.co.com/mr7vbkzZ/profile-picture-9-min.png" alt="profile" class="relative max-w-[400px] h-[400px] p-1 border-s-2 border-e-2 border-yellow-200 rounded-full" />
+        </div>
+
+        {/* <img
+          src="https://i.ibb.co.com/mr7vbkzZ/profile-picture-9-min.png"
+          className="max-w-[400px] h-[400px] p-1 border-s-2 border-e-2 shadow-blue-100 border-yellow-200  rounded-full object-cover shadow-xl"
+        /> */}
+        <div className="flex flex-col items-center md:items-start lg:w-1/2">
           <h1 className="text-5xl font-bold">
             I'm <span className="text-amber-300">Yeassin</span> Ali{' '}
           </h1>
-          <p className="my-6 pl-5 border-l-2 text-center md:text-start border-white ">
-            a Frontend Developer <br />
-            crafting responsive and user-friendly web experiences.
+          <ReactTyped
+            className="text-xl mt-2"
+            strings={['Web Developer', 'Frontend Developer', 'MERN stuck Developer']}
+            typeSpeed={90}
+            backSpeed={90}
+            loop></ReactTyped>
+          <p className="my-3 pl-5 border-l-2 text-center md:text-start border-white ">
+            I'm a passionate Frontend Developer with hands-on experience in the MERN stack (React.js, Tailwind CSS, Node.js, Express.js, MongoDB). I enjoy
+            learning new technologies and building responsive, user-friendly web applications
           </p>
-          <div className="flex items-center gap-5 text-3xl">
+          <div className="flex items-center gap-5 text-3xl mt-7">
             <a target="_blank" href="https://www.linkedin.com/in/md-yeassin-ali/">
               <FaLinkedin />
             </a>
@@ -25,9 +43,18 @@ const Hero = () => {
               <FaFacebookSquare />
             </a>
           </div>
-          <a href="/resume.pdf" download className="btn bg-gray-800 text-white">
-            Download Resume
-          </a>
+          <div className="flex items-center gap-5 mt-8">
+            <a
+              href="https://drive.google.com/file/d/11IMBWBHAKs3OwTztB269Xltd_ALcw--e/view?usp=drive_link"
+              target="_blank"
+              download
+              className="btn bg-gray-800 border-amber-200 text-white">
+              Download Resume
+            </a>
+            <a href="#contact" className="btn border-amber-200 bg-gray-800 text-white">
+              Contact
+            </a>
+          </div>
         </div>
       </div>
     </div>
